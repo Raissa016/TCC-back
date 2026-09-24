c90b2b8b4e4502908507a364ac03a749d17e1ccd
Raissa016 <raigabriela111@gmail.com>
2026-09-24 09:38:34 -0300

    Update Repositorycadastrodesecretarias.java

--- tcc-back/src/repository/Repositorycadastrodesecretarias.java
+++ tcc-back/src/repository/Repositorycadastrodesecretarias.java
@@ -1,4 +1,58 @@
 package repository;
 
-public class Repositorycadastrodesecretarias {
+import java.util.List;
+
+public class Cadastrodesecretarias{
+
+@Repository
+public class PokemonRepository {
+
+    private List<cadastrodesecretarias> listaPokemon;
+
+    public PokemonRepository(List<cadastrodesecretarias> listacadastrodesecretarias) {
+        this.listacadastrodesecretarias = listacadastrodesecretarias;
+    }
+
+
+    public Repositorycadastrodesecretarias buscarcadastrodesecretarias
+            () {
+
+        return new cadastrodesecretarias(
+                "Pikachu",
+                "Elétrico"
+        );
+
+    }
+
+    public String addcadastrodesecretarias;
+    Cadastrodesecretarias repositorycadastrodesecretarias){
+        this.listacadastrodesecretarias.add(Cadastrodesecretarias);
+
+        return "Deu certo!";
+    }
+
+    public List<cadastrodesecretarias> getLista(){
+        return this.listacadastrodesecretarias;
+    }
+
+
+    public String updatecadastrodesecretarias(String nome, cadastrodesecretarias cadastroAtualizado) {
+        for (cadastrodesecretarias cadastro : listacadastrodesecretarias) {
+            // Compara o nome ignorando maiúsculas/minúsculas
+            if (cadastro.getNome().equalsIgnoreCase(nome)) {
+
+                // Opção 1: Atualiza os dados do objeto existente (Recomendado)
+                cadastro.setNome(cadastrodesecretarias.getNome());
+                cadastro.setTipo(cadastrodesecretarias.getTipo());
+
+                return "Cadastro realizado com sucesso!";
+            }
+        }
+        return "Pokémon não encontrado.";
+    }
+
+
+
+}
+
 }
